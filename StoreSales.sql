a1a88549a1096ecc9f0cf913dba234b77a34da79
Diego Axel <diegoaxel.lopez96@gmail.com>
2024-12-06 21:59:00 -0600

    Update StoreSales.sql

--- StoreSales.sql
+++ StoreSales.sql
@@ -12,3 +12,27 @@ SELECT sub_category, state, total_sales
 FROM ranked_sales
 WHERE row_num = 1
 ORDER BY sub_category;
+
+# Los 5 estados con mayores ventas
+CREATE VIEW five_ranking_MAX AS
+SELECT state, ROUND(SUM(sales)) AS total_sales
+FROM superstore
+GROUP BY state
+ORDER BY total_sales DESC
+LIMIT 5;
+
+# Me interesa saber que estados son los que menos ventas tienen por subcategoria
+CREATE VIEW subcategory_state_sale_MIN AS
+WITH ranked_sales AS (
+    SELECT 
+        sub_category, 
+        state, 
+        ROUND(SUM(sales)) AS total_sales,
+        ROW_NUMBER() OVER (PARTITION BY sub_category ORDER BY SUM(sales) ASC) AS row_num
+    FROM superstore
+    GROUP BY sub_category, state
+)
+SELECT sub_category, state, total_sales
+FROM ranked_sales
+WHERE row_num = 1
+ORDER BY sub_category;
